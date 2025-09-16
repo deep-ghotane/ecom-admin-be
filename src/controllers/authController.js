8d99bb229804c3cd150102f4c1fcb71ddc3b142e
Yulesh Mahat <yuleshkazuto@gmail.com>
2025-09-16 13:30:51 +1000

    added password encryption

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,11 @@
 import { newAdmin } from "../models/users/userModel.js";
+import { decodeFunction, encodeFunction } from "../utils/encodeHelper.js";
 
 export const createNewUser = async (req, res) => {
-  const payload = req.body;
+  const { email, password } = req.body;
+  const hashedPassword = encodeFunction(password);
   try {
-    const admin = await newAdmin(payload);
+    const admin = await newAdmin({ email, password: hashedPassword });
     if (admin) {
       res
         .status(200)
@@ -15,5 +17,27 @@ export const createNewUser = async (req, res) => {
     }
   } catch (error) {
     res.status(500).json({ status: "error", message: "Internal server error" });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await findByFilter({ email });
+    if (user) {
+      const result = decodeFunction(password, user.password);
+      password = "";
+      if (result) {
+        res
+          .status(200)
+          .json({ status: "success", message: "Login Successful" });
+      } else {
+        res
+          .status(500)
+          .json({ status: "error", message: "Invalid credentials" });
+      }
+    }
+  } catch (error) {
+    res.status(500).json({ status: "error", message: "Internal Server error" });
   }
 };
